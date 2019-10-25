2dc48dca21ae6e76544bb5408fd561d1fbd04156
jingsu <victoriasu1994@gmail.com>
2019-10-24 21:01:32 -0400

    Add oncokb_api_bearer_token in command line and refactor main()
    Add OncoKB API in README.md
    
    Update README.md
    
    Keep workflow going on even if OncoKB API returns data failed
    
    Move API section to top
    
    Bug fix
    
    Remove getopt module
    
    add default data type for parameters

--- example.sh
+++ example.sh
@@ -9,9 +9,10 @@ IC="data/example_clinical.txt"
 OC="data/example_clinical.oncokb.txt"
 OCPDF="data/example_clinical.oncokb.pdf"
 README="data/example_README.txt"
-python MafAnnotator.py -i $IMAF -o $OMAF -c $IC
-python FusionAnnotator.py -i $IF -o $OF -c $IC
-python CnaAnnotator.py -i $ICNA -o $OCNA -c $IC
+OT="" #OncoKB API Token
+python MafAnnotator.py -i $IMAF -o $OMAF -c $IC -b $OT
+python FusionAnnotator.py -i $IF -o $OF -c $IC -b $OT
+python CnaAnnotator.py -i $ICNA -o $OCNA -c $IC -b $OT
 python ClinicalDataAnnotator.py -i $IC -o $OC -a $OMAF,$OCNA,$OF
 python OncoKBPlots.py -i $OC -o $OCPDF -c ONCOTREE_CODE #-n 10
 python GenarateReadMe.py -o $README
